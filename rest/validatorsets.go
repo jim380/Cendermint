44f457f21b05bdcbff3ff8024c6b7ba6a1de3474
jim380 <jyz1992@hotmail.com>
2021-11-03 08:13:36 +0000

    fix: bump valset pagination to 3 pages i.e. 300 validator slots

--- rest/validatorsets.go
+++ rest/validatorsets.go
@@ -30,28 +30,33 @@ type consPubKeyValSet struct {
 }
 
 func (rd *RESTData) getValidatorsets(currentBlockHeight int64) {
-	var vSets validatorsets
+	var vSets, vSets2, vSets3 validatorsets
 	var vSetsResult map[string][]string = make(map[string][]string)
+	var vSetsResult2 map[string][]string = make(map[string][]string)
+	var vSetsResult3 map[string][]string = make(map[string][]string)
 
-	res, err := RESTQuery("/validatorsets/" + fmt.Sprint(currentBlockHeight) + "?pagination.limit=1000")
-	if err != nil {
-		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", err.Error()))
-	}
-	json.Unmarshal(res, &vSets)
-	if strings.Contains(string(res), "not found") {
-		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", string(res)))
-	} else if strings.Contains(string(res), "error:") || strings.Contains(string(res), "error\\\":") {
-		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", string(res)))
-	} else {
-		zap.L().Info("", zap.Bool("Success", true), zap.String("Active validators", fmt.Sprint(len(vSets.Result.Validators))))
-	}
+	runPages(currentBlockHeight, &vSets, vSetsResult, 1)
+	runPages(currentBlockHeight, &vSets2, vSetsResult2, 2)
+	runPages(currentBlockHeight, &vSets2, vSetsResult2, 3)
 
 	for _, value := range vSets.Result.Validators {
 		// populate the validatorset map => [ConsPubKey][]string{ConsAddr, VotingPower, ProposerPriority}
 		vSetsResult[value.ConsPubKey.Key] = []string{value.ConsAddr, value.VotingPower, value.ProposerPriority, "0"}
 	}
 
-	rd.Validatorsets = Sort(vSetsResult)
+	for _, value := range vSets2.Result.Validators {
+		// populate the validatorset map => [ConsPubKey][]string{ConsAddr, VotingPower, ProposerPriority}
+		vSetsResult2[value.ConsPubKey.Key] = []string{value.ConsAddr, value.VotingPower, value.ProposerPriority, "0"}
+	}
+
+	for _, value := range vSets3.Result.Validators {
+		// populate the validatorset map => [ConsPubKey][]string{ConsAddr, VotingPower, ProposerPriority}
+		vSetsResult2[value.ConsPubKey.Key] = []string{value.ConsAddr, value.VotingPower, value.ProposerPriority, "0"}
+	}
+	vSetsResultTemp := mergeMap(vSetsResult, vSetsResult2)
+	vSetsResultFinal := mergeMap(vSetsResultTemp, vSetsResult3)
+	rd.Validatorsets = Sort(vSetsResultFinal)
+	zap.L().Info("", zap.Bool("Success", true), zap.String("Active validators", fmt.Sprint(len(vSets.Result.Validators)+len(vSets2.Result.Validators)+len(vSets3.Result.Validators))))
 }
 
 func Sort(mapValue map[string][]string) map[string][]string {
@@ -74,4 +79,31 @@ func Sort(mapValue map[string][]string) map[string][]string {
 		newMapValue[key][3] = strconv.Itoa(i + 1)
 	}
 	return newMapValue
+}
+
+func mergeMap(a map[string][]string, b map[string][]string) map[string][]string {
+	for k, v := range b {
+		a[k] = v
+	}
+	return a
+}
+
+func runPages(currentBlockHeight int64, vSets *validatorsets, vSetsResult map[string][]string, pages int) {
+	res, err := RESTQuery("/validatorsets/" + fmt.Sprint(currentBlockHeight) + "?page=" + strconv.Itoa(pages) + "&limit=100")
+	if err != nil {
+		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", err.Error()))
+	}
+
+	json.Unmarshal(res, &vSets)
+
+	if strings.Contains(string(res), "not found") {
+		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", string(res)))
+	} else if strings.Contains(string(res), "error:") || strings.Contains(string(res), "error\\\":") {
+		zap.L().Fatal("", zap.Bool("Success", false), zap.String("err", string(res)))
+	}
+
+	for _, value := range vSets.Result.Validators {
+		// populate the validatorset map => [ConsPubKey][]string{ConsAddr, VotingPower, ProposerPriority}
+		vSetsResult[value.ConsPubKey.Key] = []string{value.ConsAddr, value.VotingPower, value.ProposerPriority, "0"}
+	}
 }
